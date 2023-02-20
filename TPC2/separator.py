d24c874540e2e8c5cc6797a661b1722d36d29fa7
sucodlaranja <joaoandre210@gmail.com>
2023-02-20 20:50:44 +0000

    only missing last phase

--- TPC2/separator.py
+++ TPC2/separator.py
@@ -2,12 +2,38 @@ from bs4 import BeautifulSoup
 
 f = open('arq.xml', "r")
 
-
 soup = BeautifulSoup(f, "xml")
-print(soup.prettify().split('<ARQELEM>')[1])
 
-for index,elem in enumerate(soup.prettify().split('<ARQELEM>')):
-    if(index > 0):
-        f = open('files/arq'+str(index)+'.xml', "w")
-        elem = '<ARQELEM>'+elem
-        f.write(elem)
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Index</h1>
+            <ul>
+"""
+
+
+index = 1
+for elem in soup.find_all('ARQELEM'):
+    f = open('files/arq'+str(index)+'.xml', "w")
+    index += 1
+    f.write(str(elem))
+    f.close()
+    html += "<li><a href=\"/" + \
+        str(index) + "\">" + str(elem.find("IDENTI").text) +"</a></li>"
+
+
+html += """
+</ul>
+</body>
+</html>
+"""
+
+file = open('index.html', 'w')
+file.write(html)
+file.close()
--- TPC2/servidor.js
+++ TPC2/servidor.js
@@ -1,16 +1,23 @@
 var http = require("http");
-var url = require("url")
+var url = require("url");
 var fs = require("fs");
 
 http
   .createServer(function (req, res) {
     var number = url.parse(req.url, true).pathname.split("/")[1];
-    fs.readFile("files/arq" + number + ".xml", function (err, data) {
+    let file;
+    if (number == "") {
+      file = "index.html";
+      res.writeHead(200, { "Content-Type": "text/html" });
+    } else {
+      res.writeHead(200, { "Content-Type": "text/xml" });
+      file = "files/arq" + number + ".xml";
+    }
+    fs.readFile(file, function (err, data) {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
         res.write("Erro: " + err);
       } else {
-        res.writeHead(200, { "Content-Type": "text/xml" });
         res.write(data);
       }
       res.end();
